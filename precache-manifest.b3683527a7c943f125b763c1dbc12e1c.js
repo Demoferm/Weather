a788ad35cda49153478f101195b3eaf8aca9591b
Demoferm <60359246+Demoferm@users.noreply.github.com>
2020-04-14 17:54:02 -0400

    Updates

---
+++ precache-manifest.b3683527a7c943f125b763c1dbc12e1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ff838d180a4486f24d4e46fa782e8df",
+    "url": "/Demoferm/Weather.git/index.html"
+  },
+  {
+    "revision": "69da3c0bfc384c782552",
+    "url": "/Demoferm/Weather.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1cf05824f6f59081ac26",
+    "url": "/Demoferm/Weather.git/static/js/2.cafac32d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Demoferm/Weather.git/static/js/2.cafac32d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69da3c0bfc384c782552",
+    "url": "/Demoferm/Weather.git/static/js/main.e544f89d.chunk.js"
+  },
+  {
+    "revision": "e6f1a3b358e0a9429e9c",
+    "url": "/Demoferm/Weather.git/static/js/runtime-main.ba35882e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Demoferm/Weather.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
