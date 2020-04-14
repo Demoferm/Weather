0ded932acb9b0209d20ee81a089afd0f9b1fd294
Demoferm <60359246+Demoferm@users.noreply.github.com>
2020-04-14 18:12:21 -0400

    Updates

---
+++ precache-manifest.f986d9aad90757dff3f5f6d231f39481.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32d98ebe74afb999d16b84f1b222b2c9",
+    "url": "/Weather/index.html"
+  },
+  {
+    "revision": "c374cea168972c365734",
+    "url": "/Weather/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0845436ca75f45557a99",
+    "url": "/Weather/static/js/2.a6fcdbd0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather/static/js/2.a6fcdbd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c374cea168972c365734",
+    "url": "/Weather/static/js/main.645ecfd7.chunk.js"
+  },
+  {
+    "revision": "874ab5751dedc6122efb",
+    "url": "/Weather/static/js/runtime-main.ae02ea9b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Weather/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Demoferm/Weather.git/precache-manifest.c40e7531ecd4f5306e8480fd55417016.js"
+  "/Weather/precache-manifest.f986d9aad90757dff3f5f6d231f39481.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Demoferm/Weather.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Weather/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
