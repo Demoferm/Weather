c24033f78c8d26d0925aaf921e6226b712f8c4be
Demoferm <60359246+Demoferm@users.noreply.github.com>
2020-04-14 17:49:55 -0400

    weather

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    const [todos, setTodos] = useState({
+        main:
+            {
+                temp: ' ',
+            }
+    });
+    const [tempC, setTemp] = useState();
+
+
+    const getTodos = () => {
+        console.log('todos is getting executed')
+        // fetch('https://api.openweathermap.org/data/2.5/weather?q=Miami&APPID=23115c617732be5077c954f6e8f4d9e6')
+        //     .then(response => response.json())
+        //     .then(json => {
+        //         console.log(json)
+        //         setTodos(json);
+        //     })
+let city = document.getElementById('city').value;
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=23115c617732be5077c954f6e8f4d9e6`)
+            .then(function (response) {
+                // handle success
+                console.log(response);
+                setTodos(response.data);
+                setTemp((response.data.main.temp-273.15).toFixed(2)+' °C');
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .then(function () {
+                // always executed
+            });
+
+    }
+
+
+
+    return (
+        <div className="App">
+            <input id='city' placeholder='Enter the city'/>
+            <button onClick={getTodos}>OK</button>
+            <p>Temperature is: {tempC} </p>
+
+            {/*{*/}
+            {/*          todos.map(el => (*/}
+            {/*              <div key={el.id}>{el.title}</div>*/}
+            {/*              )*/}
+            {/*        */}
+            {/*         )*/}
+            {/*}*/}
+        </div>
+    );
 }
 
 export default App;
